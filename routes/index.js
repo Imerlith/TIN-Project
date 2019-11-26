7434bce3380accb8bdedfe371b12d3d638c22cd7
Przemysław Gołębski <emreis@vindicator.home>
2019-11-26 19:02:20 +0100

    Restructure

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Gamers denn', mainTitle: 'XxX_G4m3r$_D3nn_XxX' });
+// });
+
+router.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/../templates/index.html'))
 });
 
 module.exports = router;
